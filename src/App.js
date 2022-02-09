6b05b4b6dc86f2b05bf3c2f94421ba4107c8dce7
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-09 12:26:42 -0600

    Use grid to set calculator basic layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Header</header>
+      <body>
+        <div className="calculator">
+          <input type="text" className="calculator-screen" />
+
+          <button type="button" className="operator divide" value="/">&divide;</button>
+          <button type="button" className="operator multiply" value="*">&times;</button>
+          <button type="button" className="operator substract" value="-">-</button>
+          <button type="button" className="operator add" value="+">+</button>
+          <button type="button" className="equal-sign" value="=">=</button>
+
+          <button type="button" className="all-clear" value="all-clear">AC</button>
+          <button type="button" className="invert-sign" value="invert-sign">+/-</button>
+          <button type="button" className="percentage" value="percentage">%</button>
+
+          <button type="button" className="number-key zero" value="0">0</button>
+          <button type="button" className="number-key one" value="1">1</button>
+          <button type="button" className="number-key two" value="2">2</button>
+          <button type="button" className="number-key three" value="3">3</button>
+          <button type="button" className="number-key four" value="4">4</button>
+          <button type="button" className="number-key five" value="5">5</button>
+          <button type="button" className="number-key six" value="6">6</button>
+          <button type="button" className="number-key seven" value="7">7</button>
+          <button type="button" className="number-key eight" value="8">8</button>
+          <button type="button" className="number-key nine" value="9">9</button>
+
+          <button type="button" className="decimal" value=".">.</button>
+
+        </div>
+      </body>
     </div>
   );
 }
