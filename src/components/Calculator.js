f14581455850b8ad8e63dd64f2f9de6484ba53f2
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-10 14:21:20 -0600

    use handleClick to display numbers on Calc Screen

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,6 +10,13 @@ class Calculator extends React.Component {
       next: null,
       operation: null,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState((state) => (
+      calculate(state, event.target.value)
+    ));
   }
 
   render() {
@@ -17,30 +24,34 @@ class Calculator extends React.Component {
     return (
       <>
         <div className="calculator">
-          <input type="text" className="calculator-screen gray" value="0" disabled />
+          <div className="calculator-screen gray">
+            {total !== null ? total : null}
+            {operation !== null ? operation : null}
+            {next !== null ? next : null}
+          </div>
 
-          <button type="button" className="operator divide orange" value="/">&divide;</button>
-          <button type="button" className="operator multiply orange" value="*">&times;</button>
-          <button type="button" className="operator substract orange" value="-">-</button>
-          <button type="button" className="operator add orange" value="+">+</button>
-          <button type="button" className="equal-sign orange" value="=">=</button>
+          <button type="button" className="operator divide orange" value="/" onClick={this.handleClick}>&divide;</button>
+          <button type="button" className="operator multiply orange" value="*" onClick={this.handleClick}>&times;</button>
+          <button type="button" className="operator substract orange" value="-" onClick={this.handleClick}>-</button>
+          <button type="button" className="operator add orange" value="+" onClick={this.handleClick}>+</button>
+          <button type="button" className="equal-sign orange" value="=" onClick={this.handleClick}>=</button>
 
-          <button type="button" className="all-clear" value="all-clear">AC</button>
-          <button type="button" className="invert-sign" value="invert-sign">+/-</button>
-          <button type="button" className="percentage" value="percentage">%</button>
+          <button type="button" className="all-clear" value="all-clear" onClick={this.handleClick}>AC</button>
+          <button type="button" className="invert-sign" value="invert-sign" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="percentage" value="percentage" onClick={this.handleClick}>%</button>
 
-          <button type="button" className="number-key zero" value="0">0</button>
-          <button type="button" className="number-key one" value="1">1</button>
-          <button type="button" className="number-key two" value="2">2</button>
-          <button type="button" className="number-key three" value="3">3</button>
-          <button type="button" className="number-key four" value="4">4</button>
-          <button type="button" className="number-key five" value="5">5</button>
-          <button type="button" className="number-key six" value="6">6</button>
-          <button type="button" className="number-key seven" value="7">7</button>
-          <button type="button" className="number-key eight" value="8">8</button>
-          <button type="button" className="number-key nine" value="9">9</button>
+          <button type="button" className="number-key zero" value="0" onClick={this.handleClick}>0</button>
+          <button type="button" className="number-key one" value="1" onClick={this.handleClick}>1</button>
+          <button type="button" className="number-key two" value="2" onClick={this.handleClick}>2</button>
+          <button type="button" className="number-key three" value="3" onClick={this.handleClick}>3</button>
+          <button type="button" className="number-key four" value="4" onClick={this.handleClick}>4</button>
+          <button type="button" className="number-key five" value="5" onClick={this.handleClick}>5</button>
+          <button type="button" className="number-key six" value="6" onClick={this.handleClick}>6</button>
+          <button type="button" className="number-key seven" value="7" onClick={this.handleClick}>7</button>
+          <button type="button" className="number-key eight" value="8" onClick={this.handleClick}>8</button>
+          <button type="button" className="number-key nine" value="9" onClick={this.handleClick}>9</button>
 
-          <button type="button" className="decimal" value=".">.</button>
+          <button type="button" className="decimal" value="." onClick={this.handleClick}>.</button>
 
         </div>
       </>
