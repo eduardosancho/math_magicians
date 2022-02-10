9b118a4d30ad4ebeff0c9a5ce28b936f96b6df5c
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-10 14:09:07 -0600

    Add state variables that will update

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <>
         <div className="calculator">
