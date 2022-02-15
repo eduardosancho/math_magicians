c3c0fd9c55719201d5f67bef2766fa0d94f7189c
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-15 10:43:50 -0600

    Add routes for about and home section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 
 const App = () => (
   <div className="App">
     <header className="App-header">Header</header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
     <Calculator />
   </div>
 );
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don\`t you think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 export default App;
