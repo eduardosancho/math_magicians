deaf56e8c0520e1ec713ddc52898a41566fda07e
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-16 16:02:58 -0600

    Add test for home and calculator sections

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('Test Router behaviour', () => {
-  it('When user presses Quote it will render Quote page', () => {
+  it('When user presses Quote in navbar it will render Quote page', () => {
     const { container } = render(
       <BrowserRouter>
         <App />
@@ -13,6 +13,30 @@ describe('Test Router behaviour', () => {
     );
     const quoteLink = screen.getByText('Quote');
     fireEvent.click(quoteLink);
+    const main = container.querySelector('main');
+    expect(main).toMatchSnapshot();
+  });
+
+  it('When user presses Home in navbar it will render Home page', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const homeLink = screen.getByText('Home');
+    fireEvent.click(homeLink);
+    const main = container.querySelector('main');
+    expect(main).toMatchSnapshot();
+  });
+
+  it('When user presses Calculator in navbar it will render Calculator page', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const calculatorLink = screen.getByText('Calculator');
+    fireEvent.click(calculatorLink);
     const main = container.querySelector('main');
     expect(main).toMatchSnapshot();
   });
